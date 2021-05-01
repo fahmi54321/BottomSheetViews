07dd42f8599d21f18d7ad5922f17a40a70bf6824
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 14:56:58 +0700

    bottom sheet

--- app/src/main/java/com/android/a7bottomsheetviews/MainActivity.java
+++ app/src/main/java/com/android/a7bottomsheetviews/MainActivity.java
@@ -3,12 +3,34 @@ package com.android.a7bottomsheetviews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior bottomSheetBehavior;
+    boolean isClick = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        Button button = (Button) findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isClick == false) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }else{
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+                isClick=!isClick;
+            }
+        });
     }
 }
